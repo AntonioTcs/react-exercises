67ecd73b2b5e0a4606ce4aaa3473ab63b3532f10
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-11 13:44:04 +0200

    Feat:Exercise state-04 solved

---
+++ state-04/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: this.props.initialCountValue,
+  };
+
+  constructor(props) {
+    super(props);
+
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + this.props.incrementValue,
+        };
+      });
+    }, this.props.timerValue);
+  }
+
+  render() {
+    return <DisplayCounter count={this.state.count} />;
+  }
+}
+
+class DisplayCounter extends React.Component {
+  render() {
+    return <h1>Count: {this.props.count}</h1>;
+  }
+}
+export default Counter;
