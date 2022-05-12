70755c764a434bc7ff4813604354b802e84431f6
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-12 15:52:12 +0200

    Feat:Exercise forms-06 solved

---
+++ Forms-Lists-and-Styling/forms-06/src/index.js
@@ -0,0 +1,6 @@
+import { UncontrolledLogin } from "./App";
+import reactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+
+reactDOM.render(<UncontrolledLogin />, rootElement);
