c84e720bef54c3a37d33aa0ca21832b1009597b6
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-12 15:06:30 +0200

    Feat: Exercise forms-03 semi-solved

---
+++ Forms-Lists-and-Styling/forms-03/src/App.js
@@ -0,0 +1,61 @@
+import react from "react";
+
+export class Login extends react.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+    bntEnable: true,
+  };
+
+  InputChangeHandler = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    const type = event.target.type;
+    const check = event.target.checked;
+
+    this.setState({
+      [name]: type === "checkbox" ? check : value,
+    });
+
+    if (this.state.username && this.state.password) {
+      this.setState({
+        bntEnable: false,
+      });
+    }
+  };
+
+  ButtonClickHandler = () => {
+    this.props.onLogin(); //????
+  };
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.InputChangeHandler}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.InputChangeHandler}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          checked={this.state.remember}
+          onChange={this.InputChangeHandler}
+        />
+
+        <button
+          disabled={this.state.bntEnable}
+          onClick={this.ButtonClickHandler}
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
+}
---
+++ Forms-Lists-and-Styling/forms-03/src/index.js
@@ -0,0 +1,5 @@
+import { Login } from "./App";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Login />, rootElement);
