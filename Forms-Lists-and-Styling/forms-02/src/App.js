e66033a6616c6b0030e0d5d2ed02fde8e74b38c2
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-12 14:05:58 +0200

    Feat: Exercise forms-02 solved

---
+++ Forms-Lists-and-Styling/forms-02/src/App.js
@@ -0,0 +1,44 @@
+import react from "react";
+
+export class Login extends react.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+  };
+
+  ChangeEventHandler = (event) => {
+    const name = event.target.name;
+    const type = event.target.type;
+    const value = event.target.value;
+    const check = event.target.checked;
+
+    this.setState({
+      [name]: type === "checkbox" ? check : value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.ChangeEventHandler}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.ChangeEventHandler}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          value={this.state.remember}
+          onChange={this.ChangeEventHandler}
+        />
+      </div>
+    );
+  }
+}
---
+++ Forms-Lists-and-Styling/forms-02/src/index.js
@@ -0,0 +1,6 @@
+import { Login } from "./App";
+import reactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+
+reactDOM.render(<Login />, rootElement);
